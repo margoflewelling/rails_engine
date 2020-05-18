a94cc40d15a0f2ab9c180f4e10ab9e5553034961
Margo Flewelling <flewelling.margo@gmail.com>
2020-05-18 14:31:41 -0600

    Change relationship endpoints to utilize AR relations

--- app/controllers/api/v1/merchants/items_controller.rb
+++ app/controllers/api/v1/merchants/items_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::Merchants::ItemsController < ApplicationController
 
   def index
-    merchant_items = Item.where(merchant_id: params["id"].to_i)
-    render json: ItemSerializer.new(merchant_items).serialized_json
+    merchant = Merchant.find(params["id"])
+    render json: ItemSerializer.new(merchant.items).serialized_json
   end
 
 end
